4e31be304b81028c0c3a1652fd15c0ea46f4bc53
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-01-26 22:31:45 +0100

    first commit

---
+++ src/test/java/com/lotaproject/onlinebooktrackingsystem/OnlineBookTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lotaproject.onlinebooktrackingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookTrackingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
